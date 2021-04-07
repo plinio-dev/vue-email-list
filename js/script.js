580594bfe63808cbc2c58e6863aede1b69e48636
Tommaso P <tommaso.plinio@gmail.com>
2021-04-07 17:15:24 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
+var app = new Vue ({
+
+  el: "#app",
+  data: {
+    emails:[ ],
+
+  },
+  mounted: function(){
+    var emails = [];
+    var self = this;
+
+    for (i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(risposta){
+
+      self.emails.push(risposta.data.response)
+
+      });
+    }console.log(emails);
+  }
+
+});
